2cbc8786bd81f90df733d9ea25709385de927a11
Henrique Cabrita <hmrc_aim@hotmail.com>
2025-07-22 14:09:49 +0100

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'digital-market.henrique-cabrita.com']
 
 
 # Application definition
